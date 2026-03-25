5473f9c047514146bbbc79990135055c98355e44
Connor Thurston <cmt@bucknell.edu>
2026-03-25 15:25:02 -0400

    Finished implementation of Needleman-Wunsch

--- DNA_sequence.py
+++ DNA_sequence.py
@@ -2,8 +2,9 @@
 def interface():
     # make the interface which asks for a file_name for the query
     print("-----/ DNA Sequence Algorithm /-----")
-    print("Fill with Details about the algorithm")
-    return input("Name of the File: ")
+    print("This program takes in a file name containg \na query and finds the most similar sequence \nof DNA in the file DNA_sequences.txt")
+    print("------------------------------------")
+    return input("Name of the file containg query: ")
 
 
 def computeSim(s, t):
@@ -11,18 +12,37 @@ def computeSim(s, t):
     m = len(s)
     n = len(t)
 
-    # create dynamic programming table
+    # create dynamic programming table, inital state = all zeros
     dp_table = []
     for i in range(m+1):
         row = []
         for j in range(n+1):
-            row.append(None)
+            row.append(0)
         dp_table.append(row)
 
-    dp_table[0][0] = 0 
+    # first row and col = gap pentalities
+    for i in range(1, m+1):
+        dp_table[i][0] = dp_table[i-1][0] - 1
+    for j in range(1, n+1):
+        dp_table[i][0] = dp_table[0][j-1] - 1
+    
+    #  fill table, if the cell above and the the left are the same, increment the cell by 1, else decrement
+    for i in range(1, m + 1):
+        for j in range(1, n+1):
+            if (s[i-1] == t[j-1]):
+                match = dp_table[i-1][j-1] + 1
+            else:
+                match = dp_table[i-1][j-1] - 1
+            delete = dp_table[i-1][j] - 1
+            insert = dp_table[i][j-1] - 1
+
+            dp_table[i][j] = max(match, delete, insert)
+    
+    # number of matching nucleotides
+    return dp_table[m][n]
 
 
-def FindMostSimilarSubSequence(t, D):
+def FindMostSimilarSubSequence(D, t):
     # best similarity
     best_sim = float('-inf')
     # best sequence
@@ -33,7 +53,7 @@ def FindMostSimilarSubSequence(t, D):
         sim = computeSim(D[i], t)
         if sim > best_sim:
             best_sim = sim
-            best_seq = best_seq
+            best_seq = D[i]
     
     return best_seq, best_sim
 
@@ -48,20 +68,31 @@ def main():
 
     # open DNA_sequences.txt and read into sequences
     f_sequences = open("DNA_sequences.txt", "r")
-    sequences = f_sequences.read()
-
-    # parse the lines from the DNA_Sequences.txt file into a list
-    lines = sequences.splitlines()
+    
     dna_sequences = []
-    for line in lines:
-        line = line.strip().upper()
-        if all(c in "ACTG" for c in line) and (line != ""):
-            dna_sequences.append(line)
+    current_seq = []
+    
+    # go and parse each line, if it starts with > then it is a name and we can exclude it
+    for line in f_sequences:
+        line = line.strip()
+
+        if line.startswith(">"):
+            if current_seq:
+                dna_sequences.append("".join(current_seq))
+                current_seq = []
+        else: 
+            current_seq.append(line)
+
+    if current_seq:
+        dna_sequences.append("".join(current_seq))
+
+    #print(dna_sequences)
 
     # returns the most similar subsequence in dna_sequence to the query
-    sim, seq = FindMostSimilarSubSequence(query, dna_sequences)
-    print("Best Sim: " + sim)
-    print("Best Sequence" + seq)
+    seq, sim = FindMostSimilarSubSequence(dna_sequences, query)
+    print("DNA Query:\n" + str(query) + "\n")
+    print("Best Sequence:\n" + str(seq) + "\n")
+    print("Number of Similar Nucleotides:\n" + str(sim) + "\n")
     
 if __name__ == "__main__":
     main()
